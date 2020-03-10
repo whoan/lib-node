439370d2168712e45ac978d17fc0e32242e78cae
Juan Eugenio Abadie <juaneabadie@gmail.com>
2020-03-10 00:11:39 -0300

    Separate subscriber and pattern-subscriber

---
+++ pattern-subscriber.js
@@ -0,0 +1,31 @@
+var Redis = require('ioredis')
+
+class Psubscriber {
+  constructor (channelPrefix, msgHandler, host='redis') {
+    if (!channelPrefix) {
+      throw new Error('I need at least a channel to work')
+    }
+    if (!msgHandler) {
+      throw new Error('I need a message handler')
+    }
+
+    this.subscriber = new Redis({
+      host: host,
+      retryStrategy: function (times) {
+        var delay = Math.min(times * 500, 2000)
+        return delay
+      }
+    })
+
+    this.subscriber.on('pmessage', function (pattern, channel, info) {
+      msgHandler(channel, info)
+    })
+    this.subscriber.psubscribe(channelPrefix, function (error) {
+      if (error) {
+        throw new Error('Failed at psubscribing to channel: ' + channelPrefix)
+      }
+    })
+  }
+}
+
+module.exports = Psubscriber
--- subscriber.js
+++ subscriber.js
@@ -1,15 +1,15 @@
 var Redis = require('ioredis')
 
 class Subscriber {
-  constructor (channelPrefix, msgHandler, host='redis') {
-    if (!channelPrefix) {
-      throw new Error('I need a channel to work')
+  constructor (channels, msgHandler, host='redis') {
+    if (!channels) {
+      throw new Error('I need at least a channel to work')
     }
     if (!msgHandler) {
       throw new Error('I need a message handler')
     }
 
-    const subscriber = new Redis({
+    this.subscriber = new Redis({
       host: host,
       retryStrategy: function (times) {
         var delay = Math.min(times * 500, 2000)
@@ -17,14 +17,14 @@ class Subscriber {
       }
     })
 
-    subscriber.on('message', msgHandler)
-    subscriber.on('pmessage', function (pattern, channel, info) {
-      msgHandler(channel, info)
+    this.subscriber.on('message', function () {
+      msgHandler(...arguments)
     })
-    subscriber.psubscribe(channelPrefix + '*', function (error) {
+    this.subscriber.subscribe(...channels, function (error, count) {
       if (error) {
-        throw new Error('Failed at psubscribing to channel: ' + channelPrefix + '*')
+        throw new Error(`Failed at subscribing to ${count} channels: ` + channels.join(" "))
       }
+      console.log(`Subscribed to ${count} channels: ` + channels.join(" "))
     })
   }
 }
